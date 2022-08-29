aeaf1fbe9ecb0354117e67af24516e0e54460397
TheBoringDude <iamcoderx@gmail.com>
2022-08-30 00:23:16 +0800

    test user transaction

--- src/components/WaxUser.svelte
+++ src/components/WaxUser.svelte
@@ -6,11 +6,40 @@
 
     const { user, isLoggedIn, loginWithAnchor, logout } =
         getContext<AuthContextProps>(contextKey);
+
+    const testTransact = async () => {
+        await $user
+            .transact(
+                [
+                    {
+                        account: "eosio.token",
+                        name: "transfer",
+                        authorization: [
+                            {
+                                actor: $user.wallet,
+                                permission: $user.permission,
+                            },
+                        ],
+                        data: {
+                            from: $user.wallet,
+                            to: "eosio",
+                            quantity: "0.00000001 WAX",
+                            memo: "",
+                        },
+                    },
+                ],
+                { blocksBehind: 3, expireSeconds: 1200 }
+            )
+            .then((r) => {
+                console.log(r);
+            });
+    };
 </script>
 
 <div>
     {#if $isLoggedIn}
         <button on:click={logout}>logout</button>
+        <button on:click={testTransact}>test transaction</button>
     {:else}
         <button on:click={loginWithAnchor}>login with anchor</button>
     {/if}
